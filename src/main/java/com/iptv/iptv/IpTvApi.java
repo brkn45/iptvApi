f349a57109d870bae8e57983ba02643a0504d2dd
berkan akin <berkanakin45@hotmail.com>
2021-09-11 16:28:31 +0300

    first v1

--- src/main/java/com/iptv/iptv/IpTvApi.java
+++ src/main/java/com/iptv/iptv/IpTvApi.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IpTvApi {
 	
-	@GetMapping("/allpassenger")
+	@GetMapping("/alldata")
 	public ResponseEntity<DataEntity> pushAllPassenger(){
 		Parser parser = new Parser("berkan.m3u");
 		parser.read();
